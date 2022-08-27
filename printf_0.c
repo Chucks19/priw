fb9d088e1a8730d9e491b456839cffe84dcf8f59
Cemordi2 <emordichukwuma@gmail.com>
2022-08-27 16:43:53 +0100

    Printf

---
+++ printf_0.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+* @count: value will be returning
+* @op: value will be outputing to our screen, remeber setting an integer to print and _putchar gives number of character.
+
+
+*/
+
+int _printf(const char *format, ...)
+{
+int count, j;
+char *strin;
+unsigned int i;
+
+va_list lp;
+va_start (lp, format);
+
+// print ("Hello")
+
+for (i = 0; format[i] != '\0'; i++){
+	if (format[i] != '%')
+		count += _putchar(format[i]);
+	else if (format[i] == '%' && format[i+1] != ' ')
+	{
+		switch(format[i+1])
+		{
+			case 'c':
+				count += _putchar(va_arg(lp, int));
+				break;
+			case 's':
+				strin = va_arg(lp, char *);
+				for (j = 0; strin[j] != '\0'; j++ )
+				count += _putchar(strin[j]); 
+				break;
+			default:
+			break;
+		}
+	i++;	
+	}
+
+}
+return (count);
+}
